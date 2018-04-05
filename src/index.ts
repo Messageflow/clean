5ac0b6947342f092df20bd312f96404dbf86eade
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-04-05 23:57:30 +0800

    🚧 WIP - working on [async function del] with glob patterns

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 // @ts-check
 
+export declare interface DelParams {
+  gitConfig?: string;
+  path?: string | string[];
+  keepFileRegExp?: RegExp;
+  force?: boolean;
+}
+
 /** Import project dependencies */
 import { readFile } from 'fs';
 import { promisify } from 'util';
@@ -7,10 +14,29 @@ import { promisify } from 'util';
 /** Setting up */
 const readFrom = promisify(readFile);
 
+export const IGNORE_PATH = [
+  '.build/',
+  '.DS_Store',
+  '.esm-cache',
+  '.nyc_output',
+  '.tmp/',
+  '.vscode',
+  'npm-debug.log*',
+  'yarn-error.log*',
+  './test*/',
+  'coverage*/',
+  'dist*/',
+  'node_modules/',
+  '*.d.ts*',
+  '*.js',
+  '*.jsx',
+];
+export const REGEX_FILES_NOT_IGNORE = /(\.git|\.env)/i;
+
 export async function readGitConfig(gitConfig: string) {
   const configContent = await readFrom(gitConfig, 'utf-8');
   const globsFromContent = configContent.split(/\r?\n/i).reduce((p, n) => {
-    if (/^(#.+|$)/i.test(n)) {
+    if (/^(#.+|!.+|$)/i.test(n) || REGEX_FILES_NOT_IGNORE.test(n)) {
       return p;
     }
 
@@ -19,3 +45,23 @@ export async function readGitConfig(gitConfig: string) {
 
   return globsFromContent;
 }
+
+export async function del({
+  gitConfig,
+  path,
+  keepFileRegExp,
+  force,
+} = {} as DelParams) {
+  const config = {
+    gitConfig: gitConfig == null ? './.gitignore' : gitConfig,
+    path: path == null ? IGNORE_PATH : (Array.isArray(path) ? path : [path]),
+    keepFileRegExp: keepFileRegExp == null ? REGEX_FILES_NOT_IGNORE : keepFileRegExp,
+    force: force == null ? false : force,
+  };
+
+  const globsForDel = await readGitConfig(config.gitConfig);
+
+  return globsForDel;
+}
+
+export default del;
