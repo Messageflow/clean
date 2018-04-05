c3e45ac20d7d2079a0ae4ceda137b9fd473e7318
Rong Sen Ng <wes.ngrongsen@gmail.com>
2018-04-05 23:39:01 +0800

    🚧 WIP - working on globbing

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
 // @ts-check
 
-export async function greeting(
-  name: string = 'World'
-) {
-  try {
-    return await greetingSync(name);
-  } catch (e) {
-    throw e;
-  }
-}
+/** Import project dependencies */
+import { readFile } from 'fs';
+import { promisify } from 'util';
 
-export function greetingSync(
-  name: string = 'World'
-) {
-  return `Hello, ${name}!`;
-}
+/** Setting up */
+const readFrom = promisify(readFile);
 
-export default greeting;
+export async function readGitConfig(gitConfig: string) {
+  const configContent = await readFrom(gitConfig, 'utf-8');
+  const globsFromContent = configContent.split(/\r?\n/i).reduce((p, n) => {
+    if (/^(#.+|$)/i.test(n)) {
+      return p;
+    }
+
+    return p.concat(n);
+  }, []);
+
+  return globsFromContent;
+}
